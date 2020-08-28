e2c62d25551b26de6f8c6b8bdf0f15f136c40e4c
Ryan Parker <ryansparker@gmail.com>
2020-08-28 11:57:46 -0400

    added contact section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Menu from './components/Menu';
 import Home from './components/Home';
 import About from './components/About';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 // import logo from './logo.svg';
 import './App.css';
@@ -12,6 +13,7 @@ function App() {
     <Menu />
     <Home />
     <About />
+    <Contact />
     <Footer />
     </div>
   );

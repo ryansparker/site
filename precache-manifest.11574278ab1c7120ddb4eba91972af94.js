b2145500cb4ef9f5c986999904352085dbbec38e
Ryan Parker <ryansparker@gmail.com>
2020-08-28 12:55:22 -0400

    Updates

---
+++ precache-manifest.11574278ab1c7120ddb4eba91972af94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff738d555d44a022fefe3562625bf6d9",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "dc539a4b40a7176f5a73",
+    "url": "/site/static/css/main.a7c1aafa.chunk.css"
+  },
+  {
+    "revision": "1f9a0c71cca115154551",
+    "url": "/site/static/js/2.281675f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/site/static/js/2.281675f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc539a4b40a7176f5a73",
+    "url": "/site/static/js/main.b0fa5a1d.chunk.js"
+  },
+  {
+    "revision": "a6f832edb43cb75d95e7",
+    "url": "/site/static/js/runtime-main.6815615a.js"
+  },
+  {
+    "revision": "54b08f0f76bf0e5e4452fafc86568c55",
+    "url": "/site/static/media/windows.54b08f0f.jpg"
+  }
+]);
